c209e99a4f185496ff2aeb6b40badc1a0a25943a
xdang <xdangleterre@hotmail.com>
2025-11-24 11:37:07 +0100

    media query sur banner accomodation tag page error et accueil

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,9 +3,11 @@ import styles from"./Banner.module.scss";
 function Banner({imgSrc, alt, text=""}) {
     return(
         <div className={styles.Banner}>
-            <img src={imgSrc} alt={alt}/>
-            {text && 
-            <p>{text}</p> }
+            <div className={styles.Banner__imgWrapper} > {/* Ajout d'une div imWrapper afin de gérer le texte par rapport à l'image en mediaquery mobile  */}
+                <img src={imgSrc} alt={alt}/>
+                {text && 
+                <p>{text}</p> }
+            </div>
         </div>
     )
 
